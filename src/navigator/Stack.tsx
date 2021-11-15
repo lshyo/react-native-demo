38367ecf9aff929deaf287182b05699e4041d936
LehanChancetop <lehanlin@xm.remarkablefoods.com>
2021-11-15 04:56:00 -0500

    修改stack 首页 header样式

--- src/navigator/Stack.tsx
+++ src/navigator/Stack.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {getHeaderTitle} from '@react-navigation/elements';
 import ButtonTabs from './ButtonTabs';
 import {
   CardStyleInterpolators,
@@ -7,6 +9,7 @@ import {
   HeaderStyleInterpolators,
   StackNavigationProp,
 } from '@react-navigation/stack';
+import {StackHeaderProps} from '@react-navigation/stack/lib/typescript/src/types';
 
 export type RootStackList = {
   StackHome: undefined;
@@ -14,11 +17,23 @@ export type RootStackList = {
 export type NavigationProp = StackNavigationProp<RootStackList>;
 const Stack = createStackNavigator<RootStackList>();
 
+const MyHeader = ({navigation, route, options, back}: StackHeaderProps) => {
+  const title = getHeaderTitle(options, route.name);
+  return (
+    <View>
+      <Text style={options.headerStyle}>
+        {back ? <Text onPress={navigation.goBack}>返回</Text> : undefined}
+        {title}
+      </Text>
+    </View>
+  );
+};
 const Index = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
+          header: MyHeader,
           headerMode: 'float',
           headerTitleAlign: 'left',
           headerStyleInterpolator: HeaderStyleInterpolators.forUIKit,
